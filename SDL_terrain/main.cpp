800a9e0f4589f9dedbe2762ca7a2b67e300a49b9
Ben Keener <keener4christ@gmail.com>
2019-12-17 12:29:38 -0800

    Initial commit

--- SDL_terrain/main.cpp
+++ SDL_terrain/main.cpp
@@ -1,15 +1,58 @@
-//
-//  main.cpp
-//  SDL_terrain
-//
-//  Created by Ben on 12/17/19.
-//  Copyright © 2019 Ben. All rights reserved.
-//
+/*This source code copyrighted by Lazy Foo' Productions (2004-2019)
+ and may not be redistributed without written permission.*/
 
-#include <iostream>
+//Using SDL and standard IO
+#include <SDL2/SDL.h>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main( int argc, char* args[] )
+{
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+    SDL_Renderer *renderer = NULL;
+    
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+    
+    printf("Now running...");
+    
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+        else
+        {
+            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 0xFF, 0, 0, 0xFF);
+            SDL_RenderDrawLine(renderer, 10, 10, 100, 100 );
+            SDL_RenderPresent(renderer);
+            
+            //Wait twenty seconds
+            SDL_Delay( 5000 );
+        }
+    }
+    
+    //Destroy window
+    SDL_DestroyWindow( window );
+    
+    //Quit SDL subsystems
+    SDL_Quit();
+    
     return 0;
 }
