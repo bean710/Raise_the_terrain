4c6aa1304bbb16a712a246dc6e3c90bb4d956f80
Ben Keener <keener4christ@gmail.com>
2019-12-17 00:33:50 -0800

    Initial Commit

---
+++ SDL_terrain/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_terrain
+//
+//  Created by Ben on 12/17/19.
+//  Copyright © 2019 Ben. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
